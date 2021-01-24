5d35c6c729a78c3675080a5dd6991df087931964
megapom <megapom@localdev.local>
2021-01-25 01:54:52 +0700

    initial fonts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PagenotfoundComponent } from './pages/pagenotfound/pagenotfound.component';
+import { MainComponent } from './pages/main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'main', component: MainComponent
+    // children: [
+    //   { path: '', component: NameComponent },
+    //   { path: ':path', component: NameComponent },
+    //   { path: ':path/:params', component: NameComponent }
+    // ],
+    // canActivate: [AuthenguardGuard]
+  },
+  { path: '', pathMatch: 'full', redirectTo: 'main' },
+  { path: '**', component: PagenotfoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PagenotfoundComponent } from './pages/pagenotfound/pagenotfound.component';
+import { MainComponent } from './pages/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PagenotfoundComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
